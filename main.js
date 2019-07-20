6c4ae17dfbe3d80145dfeeebb79ca3753af67877
mmicek02 <micekmichael@gmail.com>
2019-07-19 20:00:55 -0500

    Updated CSS and JS. Fixed a few mobile display issues and got Yelp API to display brewery ratings. Still need to address number of ratings per div.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//This is for the Yelp API which is being used via a proxy server
+const apiKey= "MBJaNGZyrH0IGR_2oe_xgkfilnTgtpOvoPl6esVC7vo1s-Tj4xtM5cky8JEDlzajPNPjIhO91tTg7CnfGMXtNtZXIp3cl6OrHxpoujgqHKBS-iiJS6OKw1izb7UxXXYx"
+
 const searchURL = 'https://api.openbrewerydb.org/breweries';
 
 /* This function takes the paramaters from the user and puts it a format that can
@@ -13,14 +16,14 @@ function formatQueryParams(params) {
 /* This function will show all of the brewery results, with a maximum of ten 
 results. It displays the name of the brewery, a short description, a webiste, and
 the address */
-function displayResults(responseJson, maxResults) {
+function displayResults(responseJson, results, maxResults) {
     // if there are previous results, remove them
     console.log(responseJson);
     $('#results-list').empty();
     $('#js-error-message').empty();
     $('#js-form').removeClass('.userSearch');
     // iterate through the data array, stopping at the max number of results
-    for (let i = 0; i < responseJson.length & i<maxResults ; i++){
+    for (let i = 0; i < responseJson.length & i<maxResults; i++){
       // for each video object in the data
       //array, add a list item to the results 
       //list with the article title, source, author,
@@ -35,26 +38,60 @@ function displayResults(responseJson, maxResults) {
                     ${responseJson[i].name}
                   </h3>
                   <p class="resultText">
-                      <a href="${responseJson[i].website_url}" target="_blank">Visit this brewery's website</a>
-                    <p>${responseJson[i].street}<br>${responseJson[i].city}, ${responseJson[i].state} ${responseJson[i].postal_code}</p>
+                    <a href="${responseJson[i].website_url}" target="_blank">Visit this brewery's website</a>
+                  <p>${responseJson[i].street}<br>${responseJson[i].city}, ${responseJson[i].state} ${responseJson[i].postal_code}</p>
+                  </p>
+                  <p class="rating">
+                    
                   </p>
                 </span>
             </span>
 
             </section>`
       )};
+
     //display the results section  
     $('#results').removeClass('hidden');
   };
+  function getRatings(query, maxResults=50) {
+    const params = {
+        term: 'brewery',
+        location: query,
+        limit: maxResults,
+    };
+    const queryRatingString = formatQueryParams(params)
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const reqEndPoint = 'https://api.yelp.com/v3/businesses/search'+'?'+ queryRatingString
+    const url = proxyUrl + reqEndPoint
+
+
+    fetch(url, {headers: 
+        {Authorization: `Bearer ${apiKey}`}
+      })
+        .then(response => response.json())
+        .then(results => {
+            console.log(results)
+            //$('#results').append(JSON.stringify(results))
+            for (let p = 0; p < results.businesses.length & p<maxResults; p++){
+              console.log('show rating');
+              $('.result').parent().append(
+                `<p>
+                  Average Rating: ${results.businesses[p].rating} out of 5 stars
+                <p>`
+              )
+            };
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
 
 /* This function will parse together the user submitted data and the searchURL 
 the data can correctly and successfully pulled from the API */
 function findBrewery(query, maxResults=50) {
     const params = {
-        //by_state: query,
         by_city: query,
         per_page: maxResults,
-        //sort: 'type,name',
     };
     const queryString = formatQueryParams(params)
     const url = searchURL + '?' + queryString;
@@ -68,11 +105,13 @@ function findBrewery(query, maxResults=50) {
         }
         throw new Error(response.statusText);
       })
-      .then(responseJson => displayResults(responseJson, maxResults))
+      .then(responseJson => displayResults(responseJson, results, maxResults))
       .catch(err => {
         $('#js-error-message').text(`Something went wrong, please enter a value between 1 and 50`);
+        console.log(err)
       });
   }
+
 
 //This function will waits for the user to submit the data request to the API
 function watchForm() {
@@ -81,8 +120,22 @@ function watchForm() {
       const searchTerm = $('#js-search-term').val();
       const maxResults = $('#js-search-results').val();
       findBrewery(searchTerm, maxResults);
+      getRatings(searchTerm, maxResults);
     });
   }
 
   //Calls the watchForm function and allows the res of the code to run
   $(watchForm);
+
+
+
+/*$(() => {
+    function formatQueryParams(params) {
+        const queryItems = Object.keys(params)
+            .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        return queryItems.join('&');
+    }
+  })*/
+    
+
+
